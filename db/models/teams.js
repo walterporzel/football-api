19645c669aa131c6cb5249c47fdb0fc928f5d0d7
Walter_Porzel <walterp@vt.edu>
2019-09-11 16:40:14 -0400

    added player raw data

---
+++ db/models/teams.js
@@ -0,0 +1,14 @@
+const mongoose = require("../connection")
+
+const teamSchema = new mongoose.Schema({
+    strTeam: String,
+    strTeamShort: String,
+    strManager: String,
+    strStadium: String,
+    strStadiumLocation: String,
+    intStadiumCapacity: String,
+    strWebsite: String,
+    strDescriptionEN: String
+})
+
+module.exports = mongoose.model("Team", teamSchema)
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,19 @@
 const League = require("./models/leagues")
+const Team = require("./models/teams")
 
 const leagueSeedData = require("./leagues.json")
+const teamSeedData = require("./teams.json")
 
 League.deleteMany({}).then(() => {
     League.create(leagueSeedData).then((leagues) => {
         console.log(leagues);
+        process.exit();
+    })
+})
+
+Team.deleteMany({}).then(() => {
+    Team.create(teamSeedData).then((teams) => {
+        console.log(teams);
         process.exit();
     })
 })
