acae51e95fbc53130ef5a14bd7b067ab5fc7d665
Walter_Porzel <walterp@vt.edu>
2019-09-12 12:29:16 -0400

    added CRUD methods to player route

--- controllers/players.js
+++ controllers/players.js
@@ -7,5 +7,23 @@ module.exports = {
         .then(players =>{
             res.json(players);
         })
+    },
+    show: (req,res) => {
+        Player.findOne({strPlayer: req.params.player}).then(player =>
+            res.json(player));
+    },
+    create: (req,res) => {
+        const newPlayer = req.body;
+        Player.create(newPlayer).then(player =>
+            res.json(player));
+    },
+    update: (req,res) => {
+        const updatedPlayer = req.body;
+        Player.findOneAndUpdate({strPlayer: req.params.player},updatedPlayer, {new: true})
+        .then(player => res.json(player));
+    },
+    destroy: (req,res) => {
+        Player.findOneAndDelete({strPlayer: req.params.player})
+        .then(player => res.json(player))
     }
 }
--- routes/players.js
+++ routes/players.js
@@ -4,5 +4,9 @@ const playersRouter = express.Router();
 const playersController = require("../controllers/players");
 
 playersRouter.get('/',playersController.index);
+playersRouter.get("/:player",playersController.show);
+playersRouter.post("/",playersController.create);
+playersRouter.put('/:player',playersController.update);
+playersRouter.delete('/:player',playersController.destroy);
 
 module.exports = playersRouter;
